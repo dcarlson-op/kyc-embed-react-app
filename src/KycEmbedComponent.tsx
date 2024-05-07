7952eb0281ebf45c84357e7b44d431b55c0052b4
David Carlson <dcarlson@ontraport.com>
2024-05-07 10:19:37 -0700

    Tweak component to work for our domain

--- src/KycEmbedComponent.tsx
+++ src/KycEmbedComponent.tsx
@@ -12,39 +12,42 @@ type InboundEvent = {
   origin: string
 }
 
-type OutboundEvent = 'cancel' | 'complete' | 'ready' | 'error'
+type OutboundEvent = 'cancel' | 'complete' | 'ready' | 'error';
 
 export const KycEmbedComponent = () => {
 
   const [inquiryId, setInquiryId] = useState<string | null>(null)
   const [inquirySessionToken, setInquirySessionToken] = useState<string | null>(null)
-  const isLoading = !inquiryId && !inquirySessionToken
+  const isLoading = !inquiryId && !inquirySessionToken;
 
   useEffect(() => {
     const receiveMessage = (event: InboundEvent) => {
+      if( event.origin !== 'https://local.ontraport.com' && event.origin !== 'https://app.ontraport.com' ) {
+        return;
+      }
 
-      console.log('>>> message received', event)
-
-      // IMPORTANT: The verification below should be in place for your production code
-      // if (event.origin !== 'http://localhost:63342') return
-
-      if (!!event.data.inquiryId && !!event.data.inquirySessionToken) {
-        setInquiryId(event.data.inquiryId)
-        setInquirySessionToken(event.data.inquirySessionToken)
+      if ( !!event.data.inquiryId && !!event.data.inquirySessionToken ) {
+        setInquiryId( event.data.inquiryId );
+        setInquirySessionToken( event.data.inquirySessionToken );
       }
     }
 
-    window.addEventListener('message', receiveMessage, false)
+    window.addEventListener( 'message', receiveMessage, false );
 
     return () => {
-      window.removeEventListener('message', receiveMessage)
+      window.removeEventListener( 'message', receiveMessage );
     }
   }, [])
 
   const postEvent = (event: OutboundEvent) => {
+    // Emit event to both app and local so it works for both
     window.parent.postMessage({
       event: event,
-    }, '*') // TODO: replace this with your exact origin for security
+    }, 'https://local.ontraport.com' );
+
+    window.parent.postMessage({
+      event: event,
+    }, 'https://app.ontraport.com' );
   }
 
   return isLoading ? (
@@ -58,18 +61,14 @@ export const KycEmbedComponent = () => {
         inquirySessionToken={inquirySessionToken!}
         onCancel={() => {
           postEvent('cancel')
-          console.log('>>> cancel')
         }}
         onComplete={() => {
-          console.log('>>> complete')
           postEvent('complete')
         }}
         onReady={() => {
-          console.log('>>> ready')
           postEvent('ready')
         }}
         onError={() => {
-          console.log('>>> error')
           postEvent('error')
         }}
       />
